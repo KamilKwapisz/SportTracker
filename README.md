e313159f469f7b6ec541ff026ec4e02d7cf6a934
Kamil <you@example.com>
2018-03-26 21:33:22 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # SportTracker
-Student's  project for software engineering basis course
+
+## Built With
+
+* [Java]
+* [Swing]
+* [MSSQL]
+
+
+## Version
+
+0.1 
+
+## Authors
+
+* **Mateusz Ciupa**  
+* **Grzegorz Kopyt**  
+* **Kamil Kwapisz**  
+* **Konrad Magiera**  
+* **Daniel Sporysz**  
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE) file for details
+
---
+++ src/UserInfo.java
@@ -0,0 +1,71 @@
+public class UserInfo {
+
+    private double weight;
+    private double height;
+    private double BMI;
+    private String sex;
+
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public double getBMI() {
+        return BMI;
+    }
+
+    public void calculateBMI() {
+        try {
+            this.BMI = this.weight / ((this.height/100) * (this.height/100));
+        } catch (Exception e){
+            System.out.println("Firstly set your weight and height");
+        }
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        if( sex != "male" && sex != "female")
+                this.sex = "undefined";
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        if(this.sex == "male")
+            sb.append("he ");
+        else if(this.sex == "female")
+            sb.append("she ");
+        else
+            sb.append("it ");
+        sb.append("weights ").append(this.weight).append(" kilos and is ").append(this.height)
+                .append(" cm high. His BMI is: ").append(getBMI());
+        return sb.toString();
+    }
+
+    // main for quick testing
+    public static void main(String[] args) {
+        UserInfo ui = new UserInfo();
+        ui.setWeight(72);
+        ui.setHeight(185);
+        ui.calculateBMI();
+        ui.setSex("male");
+        System.out.println(ui);
+    }
+
+}
