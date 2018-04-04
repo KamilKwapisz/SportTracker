72c8c82e9b620bcb7e00135efd675d175c90bcff
GrzesiuKo <gak@autograf.pl>
2018-04-04 22:16:58 +0200

    Szkielecik w javafx gotowy.

---
+++ SportTrackerJavaFX/SportTrackerProject/src/main/java/Controllers/MenuController.java
@@ -0,0 +1,32 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+
+public class MenuController {
+	
+	@FXML
+	public void openProfile() {
+		
+	}
+	
+	@FXML
+	public void openStatistics(){
+		
+	}
+	
+	@FXML
+	public void openHistory(){
+		
+	}
+	
+	@FXML
+	public void openSportPlan(){
+		
+	}
+	
+	@FXML
+	public void openCalendar(){
+		
+	}
+	
+}
