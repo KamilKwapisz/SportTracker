bb07c571694267e2f9508a323857a0756eaf936d
Kamil <you@example.com>
2018-04-04 22:35:26 +0200

    got rid of training variable in statistics

--- src/Day.java
+++ src/Day.java
@@ -24,9 +24,6 @@ public class Day {
         this.training = training;
     }
 
-    public void removeTraining(){
-        this.training = 0; // tymczasowo
-    }
 
     @Override
     public String toString() {
