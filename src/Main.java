f3b0c9374b13d6f45da1758b2e47bc21e43dd888
KonradMagiera <k.magiera1@wp.pl>
2018-04-17 18:05:54 +0200

    Redesign gui
    Fresh gui. Display everything in 'display' pane

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,46 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 import javafx.application.Application;
+
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+
+import javafx.scene.layout.AnchorPane;
+
 import javafx.stage.Stage;
 
+/**
+ *
+ * @author Game
+ */
 public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(this.getClass().getResource("MainScreen.fxml"));
+        AnchorPane aPane = loader.load();
+        MainScreenController mainC = loader.getController();
+        
+        
+        
+        Scene scene = new Scene(aPane, 800, 600);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("SportTracker");
+                primaryStage.setResizable(false);
+		primaryStage.show();
+    }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
-
-	@Override
-	public void start(Stage stage) throws Exception {
-		FXMLLoader loader = new FXMLLoader(this.getClass().getResource("MainScreen.fxml"));
-		StackPane stackPane = loader.load();
-		Scene scene = new Scene(stackPane, 400, 400);
-
-		stage.setScene(scene);
-		stage.setTitle("SportTracker");
-		stage.show();
-
-	}
-
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
 }
--- src/MainScreenController.java
+++ src/MainScreenController.java
@@ -1,38 +1,77 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 import java.io.IOException;
-
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
-public class MainScreenController {
-	@FXML
-	private StackPane mainScreen;
+/**
+ * FXML Controller class
+ *
+ * @author Game
+ */
+public class MainScreenController implements Initializable{
 
-	@FXML
-	public void initialize() {
-		loadMenu();
-		
+    @FXML
+    private Button bProfile;
+    
+    @FXML
+    private AnchorPane display;
+    
+   // ProfileController profileController;
+    public MainScreenController(){
+    }
+    
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    @FXML
+    public void showProfile() throws IOException{
+     display.getChildren().clear();
+     AnchorPane pane = FXMLLoader.load(getClass().getResource("Profile.fxml"));
+     display.getChildren().setAll(pane);
+      
 	}
 
-	public void loadMenu() {
-		FXMLLoader loader = new FXMLLoader(this.getClass().getResource("Menu.fxml"));
-		Pane pane = null;
-		try {
-			pane = loader.load();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		setScene(pane);
-		MenuController menuController = loader.getController();
-		menuController.setMainScreenController(this);
-	}
+    @FXML
+    public void showStat() throws IOException{
+     display.getChildren().clear();
+     AnchorPane pane = FXMLLoader.load(getClass().getResource("Statistics.fxml"));
+     display.getChildren().setAll(pane);
+        }
+    
+    @FXML
+    public void showHistory() throws IOException{
+     display.getChildren().clear();
+     AnchorPane pane = FXMLLoader.load(getClass().getResource("History.fxml"));
+     display.getChildren().setAll(pane);
+        }
+    
+    @FXML
+    public void showSport() throws IOException{
+     display.getChildren().clear();
+     AnchorPane pane = FXMLLoader.load(getClass().getResource("Sport.fxml"));
+     display.getChildren().setAll(pane);
+        }
+    
+    @FXML
+    public void showCalendar() throws IOException{
+     display.getChildren().clear();
+     AnchorPane pane = FXMLLoader.load(getClass().getResource("Calendar.fxml"));
+     display.getChildren().setAll(pane);
+        }
+    }
 
-	public void setScene(Pane pane) {
-		mainScreen.getChildren().clear();
-		mainScreen.getChildren().add(pane);
-	}
-
-}
