f3d7fa2ee4ca770539581d8f2cdfd97fc984d6b0
GrzesiuKo <gak@autograf.pl>
2018-04-18 17:20:21 +0200

    GUI2.0

---
+++ GUISportTrackerProject/src/main/java/TextFields/NameTextField.java
@@ -0,0 +1,11 @@
+package TextFields;
+
+import javafx.scene.control.TextField;
+
+public class NameTextField extends TextField{
+	
+	public NameTextField() {
+			this.setPromptText("Enter your username :)");	
+		}
+	
+}
---
+++ GUISportTrackerProject/src/main/java/TextFields/WeightTextField.java
@@ -0,0 +1,20 @@
+package TextFields;
+
+import javafx.scene.control.TextField;
+
+public class WeightTextField extends TextField{
+	public WeightTextField() {
+		this.setPromptText("Enter your weight in kilograms :)");
+	}
+	
+	@Override
+	public void replaceText(int i, int i1,String string) {
+		if(string.matches("[0-9]") || string.isEmpty())
+			super.replaceText(i, i1, string);
+	}
+	
+	@Override
+	public void replaceSelection(String string) {
+		super.replaceSelection(string);
+	}
+}
